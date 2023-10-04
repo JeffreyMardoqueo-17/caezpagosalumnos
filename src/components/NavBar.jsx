0a5cda207dce699a5a3a6d0cac2ec6f8a450ad93
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-10-03 22:22:43 -0600

    Primeros cambios para hacer el dashBoard

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavBarr() {
+  return (
+    <div className='bg-[#000] h-screen'>
+      sa
+    </div>
+  )
+}
+
+export default NavBarr
---
+++ src/components/NavbarData.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Home from "@mui/icons-material/Home";
+export const NavBarData = [
+    {
+        title: "home",
+        // icono: <Home></Home>,
+        link: '/home'
+
+    },
+     {
+        title: "About",
+        // icono: <Home/>,
+        link: '/home'
+
+    }, {
+        title: "Contacto",
+        // icono: <Home/>,
+        link: '/home'
+
+    },
+]
